33774513f6af92f5d9aa8469a6037cb357443d60
krillzon <43472917+krillzon@users.noreply.github.com>
2019-05-06 23:21:03 +0200

    Add files via upload

--- Klockapp/javascript.js
+++ Klockapp/javascript.js
@@ -8,11 +8,30 @@ fetch(uri)
 
 .then(function(data)
 {
-        let klocka = data.timezone;
-    
-        return klocka.map(function(klockobj)
-    {
-        boi.innerHTML += `Klockan e ${klockobj.datetime}`;         
+        let time = data;
+
+        
+        if (time.day_of_week == "1"){
+            boi.innerHTML+= `Idag är det dag  (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Måndag!<br><br>`
+        } if (time.day_of_week == "2"){
+            boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Tisdag!<br><br>`
+        } if (time.day_of_week == "3"){
+            boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Onsdag!<br><br>`
+        } if (time.day_of_week == "4"){
+            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en Torsdag!<br><br>`
+        } if (time.day_of_week == "5"){
+            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en FREDAG!<br><br>`
+        } if (time.day_of_week == "6"){
+            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en Lördag!<br><br>`
+        } if (time.day_of_week == "7"){
+            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en Söndag!<br><br>`
+        }
+
+        
+        boi.innerHTML += `I ${time.timezone} så är klockan: ${time.datetime.slice(11,25)}<br>`;
+        boi.innerHTML += `Datum: ${time.datetime.slice(0,10)} <br>`;
+        boi.innerHTML += `Dagar in på året: ${time.day_of_year}<br>`;
+    boi.innerHTML += `Dagar kvar tills NYÅR!: ${365 - time.day_of_year}<br>`;
+        
             
 })    
-})
--- WeatherApi/javascript.js
+++ WeatherApi/javascript.js
@@ -1,4 +1,4 @@
-var uri = "https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22";
+var uri = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=7a8375b117e125294bdcb5a4ba2bb9d9";
 
 const krippa = document.getElementById('weather');
 
@@ -8,11 +8,35 @@ fetch(uri)
 
 .then(function(data)
 {
-        let veder = data.weather;
+        let name = data;
+        let weather = data.weather[0];
+        let temp = data.main;
+        let wind = data.wind;
     
-        return veder.map(function(weatherobj)
-    {
-        krippa.innerHTML += `Just nu är det: ${weatherobj.main}<br>`;
-        krippa.innerHTML += `I luften är det: ${weatherobj.description}<br>`;
-})  
+        if (weather.main == "Rain"){
+             krippa.innerHTML += "<img src='https://i.pinimg.com/originals/4e/78/58/4e7858e916e149f38c3d80056fcb2430.gif'><br>";
+        }   
+        if (weather.main == "Clouds") {
+           krippa.innerHTML += "<img src='https://media.giphy.com/media/xT9IgC2RzpbE7vBZ6M/giphy.gif'><br>";
+        }
+        if (weather.main == "Clear sky") { 
+            krippa.innerHTML += "<img src='https://media2.giphy.com/media/qZohEEh4bhuQ8/giphy.gif'><br>";
+        }
+        
+       
+        krippa.innerHTML += `In ${name.name} there is ${weather.main}.<br>`;
+        krippa.innerHTML += `Description of weather: ${weather.description}.<br>`;
+        krippa.innerHTML += `Wind speed: ${wind.speed}m/s<br>`
+        
+        krippa.innerHTML += `Humidity: ${temp.humidity}%<br>`
+        krippa.innerHTML += `Pressure: ${temp.pressure}<br>`
+        krippa.innerHTML += `Live Temperature: ${temp.temp}<br>`
+        krippa.innerHTML += `(Max temp: ${temp.temp_max}, min temp: ${temp.temp_min})<br>`;
+
+    
+    
 })
+
+
+
+// &appid=7a8375b117e125294bdcb5a4ba2bb9d9
