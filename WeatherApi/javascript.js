bfe05bb946aac7abd06ba9ae06ec2473a63af154
Christoffer Stathin <43472917+krillzon@users.noreply.github.com>
2019-05-09 14:21:21 +0200

    Add files via upload
    Changed name from Clear sky to Clear to print gif

--- WeatherApi/javascript.js
+++ WeatherApi/javascript.js
@@ -1,3 +1,14 @@
+
+
+
+
+
+
+
+
+
+
+
 var uri = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=7a8375b117e125294bdcb5a4ba2bb9d9";
 
 const krippa = document.getElementById('weather');
@@ -19,7 +30,7 @@ fetch(uri)
         if (weather.main == "Clouds") {
            krippa.innerHTML += "<img src='https://media.giphy.com/media/xT9IgC2RzpbE7vBZ6M/giphy.gif'><br>";
         }
-        if (weather.main == "Clear sky") { 
+        if (weather.main == "Clear") { 
             krippa.innerHTML += "<img src='https://media2.giphy.com/media/qZohEEh4bhuQ8/giphy.gif'><br>";
         }
         
@@ -29,9 +40,9 @@ fetch(uri)
         krippa.innerHTML += `Wind speed: ${wind.speed}m/s<br>`
         
         krippa.innerHTML += `Humidity: ${temp.humidity}%<br>`
-        krippa.innerHTML += `Pressure: ${temp.pressure}Pa<br>`
-        krippa.innerHTML += `Live Temperature: ${temp.temp - 273}°<br>`
-        krippa.innerHTML += `(Max temp: ${temp.temp_max - 273}°, min temp: ${temp.temp_min - 273}°)<br>`;
+        krippa.innerHTML += `Pressure: ${temp.pressure}<br>`
+        krippa.innerHTML += `Live Temperature: ${temp.temp}<br>`
+        krippa.innerHTML += `(Max temp: ${temp.temp_max}, min temp: ${temp.temp_min})<br>`;
 
     
     
