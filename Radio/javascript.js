de1abb61aeef785bfc99d7598b0cd7c6e54bf918
Christoffer Stathin <43472917+krillzon@users.noreply.github.com>
2019-05-09 16:01:37 +0200

    Add files via upload
    Added "Playing now"

--- Radio/javascript.js
+++ Radio/javascript.js
@@ -14,11 +14,10 @@ fetch(uri)
 {
         let copyright = data;
         let info = data.channels[4].playlists.playlist.previoussong;
-        //let playing = data.channels[4].playlists.playlist.song;
         let name = data.channels[4];
 
         doci.innerHTML += `COPYRIGHT: ${copyright.copyright}<br><br>`;
-        //doci.innerHTML += `Spelas just nu!: ${playing.title}<br>`;
+        
         doci.innerHTML += `Kanal: ${name.name}<br>`;
         doci.innerHTML += `Senaste låten som spelades: ${info.description}<br>`;
         doci.innerHTML += `Artist: ${info.artist}<br>`;
@@ -27,29 +26,35 @@ fetch(uri)
 
         doci.innerHTML += `<br><br><br><br>`;
         let info1 = data.channels[5].playlists.playlist.previoussong;
-        let playing1 = data.channels[5].playlists.playlist.song;
         let name1 = data.channels[5];
 
+
         doci.innerHTML += `Kanal: ${name1.name}<br>`;
-        doci.innerHTML += `Spelas just nu!: ${playing1.title}<br>`;
         doci.innerHTML += `Senaste låten som spelades: ${info1.description}<br>`;
         doci.innerHTML += `Artist: ${info1.artist}<br>`;
         doci.innerHTML += `Albumnamn: ${info1.albumname}<br>`;
         doci.innerHTML += `Skivbolag: ${info1.recordlabel}<br>`;
-     
 
         doci.innerHTML += `<br><br><br><br>`;
         let info2 = data.channels[9].playlists.playlist.previoussong;
-        let playing2 = data.channels[9].playlists.playlist.song;
         let name2 = data.channels[9];
 
+
         doci.innerHTML += `Kanal: ${name2.name}<br>`;
-        doci.innerHTML += `Spelas just nu!: ${playing2.title}<br>`;
         doci.innerHTML += `Senaste låten som spelades: ${info2.description}<br>`;
         doci.innerHTML += `Artist: ${info2.artist}<br>`;
         doci.innerHTML += `Albumnamn: ${info2.albumname}<br>`;
-        doci.innerHTML += `Skivbolag: ${info2.recordlabel}<br>`;
-    
+        doci.innerHTML += `Skivbolag: ${info2.recordlabel}<br><br>`;
+
+
+        let playing1 = data.channels[5].playlists.playlist.song;
+        let playing2 = data.channels[9].playlists.playlist.song;
+
+        
+        doci.innerHTML += `Spelas just nu på "P2" : ${playing1.title}<br>`; 
+        doci.innerHTML += `Spelas just nu på "P2 Språk och Musik" : ${playing2.title}<br>`;
+        
+   
     
   
 })
