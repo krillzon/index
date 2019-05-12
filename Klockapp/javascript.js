2cc40e143e05a2a1c53f5902e2925192bcc8a38f
Christoffer Stathin <43472917+krillzon@users.noreply.github.com>
2019-05-12 17:10:40 +0200

    Added reload function, hotfix

--- Klockapp/javascript.js
+++ Klockapp/javascript.js
@@ -10,24 +10,29 @@ fetch(uri)
 {
         let time = data;
 
-        
-        if (time.day_of_week == "1"){
+    
+         if (time.day_of_week == "1"){
             boi.innerHTML+= `Idag är det dag  (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Måndag!<br><br>`
         } if (time.day_of_week == "2"){
             boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Tisdag!<br><br>`
         } if (time.day_of_week == "3"){
             boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Onsdag!<br><br>`
         } if (time.day_of_week == "4"){
-            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en Torsdag!<br><br>`
+            boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Torsdag!<br><br>`
         } if (time.day_of_week == "5"){
-            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en FREDAG!<br><br>`
+            boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en FREDAG!<br><br>`
         } if (time.day_of_week == "6"){
-            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en Lördag!<br><br>`
-        } if (time.day_of_week == "7"){
-            boi.innerHTML+= `Idag är det dag ${time.day_of_week}) i vecka (${time.week_number}) vilket är en Söndag!<br><br>`
+            boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Lördag!<br><br>`
+        } if (time.day_of_week == "0"){
+            boi.innerHTML+= `Idag är det dag (${time.day_of_week}) i vecka (${time.week_number}) vilket är en Söndag!<br><br>`
         }
 
-        
+        /*
+        setTimeout(function() {
+            location.reload();
+          }, 1000)
+*/
+
         boi.innerHTML += `I ${time.timezone} så är klockan: ${time.datetime.slice(11,25)}<br>`;
         boi.innerHTML += `Datum: ${time.datetime.slice(0,10)} <br>`;
         boi.innerHTML += `Dagar in på året: ${time.day_of_year}<br>`;
