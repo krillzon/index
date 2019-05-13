fb0f3bc0f5839d5b260a674c5b51fbf76adc58a2
Christoffer Stathin <43472917+krillzon@users.noreply.github.com>
2019-05-13 23:05:07 +0200

    Add files via upload

--- post.js
+++ post.js
@@ -16,7 +16,7 @@ const uru = 'https://5cd15d84d4a78300147be92e.mockapi.io/isAtTheGym';
           let divtag = document.createElement('div');
 
           // Här har vi lagt till tre ytterligare länkar
-          divtag.innerHTML = `${data.id} - ${data.name} - ${data.isAtTheGym==true?"På Gymmet":"Inte På Gymmet"} <a href="#" onclick="deleteFunction(${data.id})">Radera</a>`;
+          divtag.innerHTML = `${data.id} - ${data.name} - ${data.isAtTheGym==true?"På Gymmet":"Inte på gymmet"} <a href="#" onclick="changeFunction(true, ${data.id})">På gymmet igen<br></a> <a href="#" onclick="changeFunction(false, ${data.id})">Inte på gymmet längre<br></a> <a href="#" onclick="deleteFunction(${data.id})">Radera</a>`;
           datadiv.appendChild(divtag);
         })
       });
@@ -50,6 +50,35 @@ const uru = 'https://5cd15d84d4a78300147be92e.mockapi.io/isAtTheGym';
   } 
 
 
+    // Den här funktionen körs då man klickar på någon av knapparna 'Hemma' och 'Går ut' vid varje namn.
+    // Funktionen är nästan identisk med homeFunction, men har ett extra invärde 'id'
+    // Istället för POST gör vi istället en PUT.
+    // vi lägger till id till den ursprungliga URIn.
+    function changeFunction(isAtTheGymNow, id) {
+      var data = {isAtTheGym: isAtTheGymNow }
+
+      fetch(uru + "/" + id,
+        {
+          method: "PUT", // *GET, POST, PUT, DELETE, etc.
+
+          headers:
+          {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        }).then(res => res.json())
+        .then(res => console.log(res))
+
+        // Eftersom MockAPI har en liten fördröjning så låter vi sidan uppdateras först efter 
+        // en halv sekund.
+      setTimeout(() => {
+        loadData();
+      }, 500);
+
+    }
+
+
   // Den här funktionen körs då man klickar på Radera.
   // Funktionen är nästan identisk med homeFunction, men har ett extra invärde 'id'
   // Istället för POST gör vi istället en DELETE.
@@ -64,10 +93,6 @@ const uru = 'https://5cd15d84d4a78300147be92e.mockapi.io/isAtTheGym';
       {
         // Nytt!
         method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit, 
-
         headers:
         {
           'Accept': 'application/json, text/plain, */*',
